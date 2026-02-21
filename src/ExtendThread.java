0dab562084cec1225c2aac7af155495148dcdf98
abhinavprasanth06-blip <abhinavprasanth06@gmail.com>
2026-02-21 15:23:35 +0530

    Update ExtendThread.java

--- src/ExtendThread.java
+++ src/ExtendThread.java
@@ -2,10 +2,11 @@ class MyThread extends Thread {
     public void run() {
         System.out.println("Thread is running");
     }
+}
 public class ExtendThread {
      public static void main(String[] args) {
         MyThread t = new MyThread();
         t.start();
     }
 }
-}
+
